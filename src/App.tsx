c75ec92d2f57ab1f864fb4728a0d35e075e9c503
JasanBadell <informaticoelineas3@gmail.com>
2026-03-04 14:09:29 -0500

    Parametrizar descansos y cambiar reset de contraseña a email

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppRouterBoundary } from '@/components/AppRouterBoundary';
 import { AuthProvider } from "@/contexts/AuthContext";
 import { NotificationsProvider } from "@/contexts/NotificationsContext";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
@@ -33,6 +34,7 @@ const App = () => (
         <Sonner />
         <NotificationsProvider>
         <BrowserRouter>
+          <AppRouterBoundary>
           <Routes>
             <Route path="/auth" element={<Auth />} />
             <Route
@@ -149,6 +151,7 @@ const App = () => (
             />
             <Route path="*" element={<NotFound />} />
           </Routes>
+          </AppRouterBoundary>
         </BrowserRouter>
         </NotificationsProvider>
       </TooltipProvider>
